b4b8b3b693b70cead419d2fb6d7fcc91816ad8d2
Dipen Ved <vedipen@gmail.com>
2017-01-23 03:28:45 +0530

    Add standard C grammar

--- LexicalAnalyser.java
+++ LexicalAnalyser.java
@@ -2,12 +2,42 @@ import java.util.*;
 import java.io.*;
 
 class dotSlash {
+    public Set<String> keyWords = new HashSet<String> () {{
+       add("include");
+       add("int");
+       add("main");
+       add("return");
+    }};
+    public Set<String> inbuiltFunctions = new HashSet<String> () {{
+        add("printf");
+        add("scanf");
+    }};
+    public Set<String> specialSymbols = new HashSet<String> () {{
+        add("(");
+        add(")");
+        add("{");
+        add("}");
+        add("<");
+        add(">");
+        add("#");
+        add(";");
+        add("+");
+        add("\"");
+        add(",");
+    }};
+    public Set<String> headerFiles = new HashSet<String> () {{
+        add("stdio.h");
+    }};
     public static void main (String[] args) throws IOException {
         FileInputStream fstream = new FileInputStream("input.c");
         BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
         String input;
         while((input=br.readLine()) != null) {
-            System.out.println(input);
+            StringTokenizer in = new StringTokenizer(input, "\t(){}<>#;+\",");
+            while(in.hasMoreElements()) {
+                System.out.println(in.nextToken());
+            }
+            //System.out.println(input);
         }
         br.close();
     }
